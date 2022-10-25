f1dbf5c634d043906b7725b24f0be42a583611c0
Brandon <brandonladouceur17@gmail.com>
2022-10-25 09:26:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lab6/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.bhuvan.helloworld;
+
+
+import android.os.Bundle;
+
+public class DisplayCat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_cat);
+
+    }
+}
